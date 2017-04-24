299c0ef8003849fcee58baf56a25cde663bbd553
losaped <losaped@gmail.com>
2017-04-24 13:40:31 +0300

    решение очередной проблемы с определением типа и версии

--- expinf.go
+++ expinf.go
@@ -16,16 +16,24 @@ type ExportInfo struct {
 	Version string
 }
 
-// GetExportInfo извлекает версию и тип документа
-func GetExportInfo(xmlContent string) (*ExportInfo, error) {
-	r, err := regexp.Compile(`<([a-z]+\d?[:]{1})?([a-zA-Z]*\d*[a-zA-Z]*[\S]).*(\sschemeVersion=\"(.+?)\").*>`)
-	if err != nil {
-		return nil, err
-	}
+var (
+	findTagRegex, infoRegex *regexp.Regexp
+)
 
-	results := r.FindStringSubmatch(xmlContent)
+func init() {
+	findTagRegex, _ = regexp.Compile(`.+(<.+ schemeVersion=".+".*>)`)
+	infoRegex, _ = regexp.Compile(`<([a-z]+\d?[:]{1})?([a-zA-Z]*\d*[a-zA-Z]*[\S]).*(\sschemeVersion=\"(.+?)\").*>`)
+}
+
+// GetExportInfo извлекает версию и тип документа
+func GetExportInfo(xml string) (*ExportInfo, error) {
+	tag := findTagRegex.FindStringSubmatch(xml)
+	if len(tag) < 2 {
+		return nil, fmt.Errorf("Неверный формат файла. Не удалось определить тег с версией и типом документа")
+	}
+	results := infoRegex.FindStringSubmatch(tag[1])
 	if len(results) < 5 {
-		return nil, fmt.Errorf("Wrong format export file, could not determine version and title")
+		return nil, fmt.Errorf("Неверный формат файла. Не удалось определить версию и номер документа")
 	}
 
 	return &ExportInfo{Title: results[2], Version: results[4]}, nil
--- export_info_test.go
+++ export_info_test.go
@@ -14,38 +14,45 @@ type Tst struct {
 func TestGetExportInfo(t *testing.T) {
 	tests := []Tst{
 		Tst{
-			Input: `<ns2:control99ProtocolMismatch schemeVersion="7.0">`,
+			Input: `<?xml version="1.0" encoding="UTF-8"?><export xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns="http://zakupki.gov.ru/oos/export/1" xmlns:oos="http://zakupki.gov.ru/oos/types/1"><ns2:control99ProtocolMismatch schemeVersion="7.0">`,
 			Expected: ExportInfo{
 				Title:   "control99ProtocolMismatch",
 				Version: "7.0",
 			},
 		},
 		Tst{
-			Input: `<ns2:sketchPlan schemeVersion="1.0" xmlns="http://zakupki.gov.ru/oos/types/1" xmlns:ns2="http://zakupki.gov.ru/oos/printform/1">`,
+			Input: `<?xml version="1.0" encoding="UTF-8"?><export xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns="http://zakupki.gov.ru/oos/export/1" xmlns:oos="http://zakupki.gov.ru/oos/types/1"><ns2:sketchPlan schemeVersion="1.0" xmlns="http://zakupki.gov.ru/oos/types/1" xmlns:ns2="http://zakupki.gov.ru/oos/printform/1">`,
 			Expected: ExportInfo{
 				Title:   "sketchPlan",
 				Version: "1.0",
 			},
 		},
 		Tst{
-			Input: `<ns2:fcsPurchaseDocument schemeVersion="4.2" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">`,
+			Input: `<?xml version="1.0" encoding="UTF-8"?><export xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns="http://zakupki.gov.ru/oos/export/1" xmlns:oos="http://zakupki.gov.ru/oos/types/1"><ns2:fcsPurchaseDocument schemeVersion="4.2" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">`,
 			Expected: ExportInfo{
 				Title:   "fcsPurchaseDocument",
 				Version: "4.2",
 			},
 		},
 		Tst{
-			Input: `<fcsPurchaseDocument schemeVersion="4.2" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">`,
+			Input: `<?xml version="1.0" encoding="UTF-8"?><export xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns="http://zakupki.gov.ru/oos/export/1" xmlns:oos="http://zakupki.gov.ru/oos/types/1"><fcsPurchaseDocument schemeVersion="4.2" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">`,
 			Expected: ExportInfo{
 				Title:   "fcsPurchaseDocument",
 				Version: "4.2",
 			},
 		},
 		Tst{
-			Input: `<ns2:contractProcedure xsi:type="ns3:zfcs_contractProcedureType" schemeVersion="4.5" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">`,
+			Input: `<?xml version="1.0" encoding="UTF-8"?><export xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns="http://zakupki.gov.ru/oos/export/1" xmlns:oos="http://zakupki.gov.ru/oos/types/1"><ns2:contractProcedure xsi:type="ns3:zfcs_contractProcedureType" schemeVersion="4.5" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">`,
 			Expected: ExportInfo{
 				Title:   "contractProcedure",
 				Version: "4.5",
+			},
+		},
+		Tst{
+			Input: `<?xml version="1.0" encoding="UTF-8"?><export xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns="http://zakupki.gov.ru/oos/export/1" xmlns:oos="http://zakupki.gov.ru/oos/types/1"><tenderPlan schemeVersion="1.0">`,
+			Expected: ExportInfo{
+				Title:   "tenderPlan",
+				Version: "1.0",
 			},
 		},
 	}
