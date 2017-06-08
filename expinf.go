7a06e6c55afbc4525d1eed1f7503d20f4f6009b5
losaped <losaped@gmail.com>
2017-06-08 17:16:15 +0300

    добавил функцию для нахождения версии по тегу

--- expinf.go
+++ expinf.go
@@ -21,7 +21,7 @@ var (
 )
 
 func init() {
-	findTagRegex, _ = regexp.Compile(`.+(<.+ schemeVersion=".+".*>)`)
+	// findTagRegex, _ = regexp.Compile(`.+(<.+ schemeVersion=".+".*>)`)
 	infoRegex, _ = regexp.Compile(`<([a-z]+\d?[:]{1})?([a-zA-Z]*\d*[a-zA-Z]*[\S]).*(\sschemeVersion=\"(.+?)\").*>`)
 }
 
@@ -32,6 +32,19 @@ func GetExportInfo(xml string) (*ExportInfo, error) {
 		return nil, fmt.Errorf("Неверный формат файла. Не удалось определить тег с версией и типом документа")
 	}
 	results := infoRegex.FindStringSubmatch(tag[1])
+	if len(results) < 5 {
+		return nil, fmt.Errorf("Неверный формат файла. Не удалось определить версию и номер документа")
+	}
+
+	return &ExportInfo{Title: results[2], Version: results[4]}, nil
+}
+
+func GetExportInfoByTag(tag string) (*ExportInfo, error) {
+	// tag := findTagRegex.FindStringSubmatch(xml)
+	// if len(tag) < 2 {
+	// 	return nil, fmt.Errorf("Неверный формат файла. Не удалось определить тег с версией и типом документа")
+	// }
+	results := infoRegex.FindStringSubmatch(tag)
 	if len(results) < 5 {
 		return nil, fmt.Errorf("Неверный формат файла. Не удалось определить версию и номер документа")
 	}
--- export_info_test.go
+++ export_info_test.go
@@ -62,6 +62,91 @@ func TestGetExportInfo(t *testing.T) {
 				Version: "1.0",
 			},
 		},
+		Tst{
+			Input: `<contract schemeVersion="4.5">`,
+			Expected: ExportInfo{
+				Title:   "contract",
+				Version: "4.5",
+			},
+		},
+	}
+
+	for i, test := range tests {
+		got, err := GetExportInfo(test.Input)
+		fmt.Printf("%d\n", i)
+		if err != nil {
+			if test.IsError {
+				return
+			}
+
+			t.Error(err)
+		}
+
+		if *got != test.Expected {
+			t.Errorf("got %v != Expected %v", *got, test.Expected)
+			t.Fail()
+		}
+	}
+}
+
+func TestGetExportInfo2(t *testing.T) {
+	tests := []Tst{
+		Tst{
+			Input: `<ns2:control99ProtocolMismatch schemeVersion="7.0">`,
+			Expected: ExportInfo{
+				Title:   "control99ProtocolMismatch",
+				Version: "7.0",
+			},
+		},
+		Tst{
+			Input: `<ns2:sketchPlan schemeVersion="1.0" xmlns="http://zakupki.gov.ru/oos/types/1" xmlns:ns2="http://zakupki.gov.ru/oos/printform/1">`,
+			Expected: ExportInfo{
+				Title:   "sketchPlan",
+				Version: "1.0",
+			},
+		},
+		Tst{
+			Input: `<ns2:fcsPurchaseDocument schemeVersion="4.2" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">`,
+			Expected: ExportInfo{
+				Title:   "fcsPurchaseDocument",
+				Version: "4.2",
+			},
+		},
+		Tst{
+			Input: `<fcsPurchaseDocument schemeVersion="4.2" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">`,
+			Expected: ExportInfo{
+				Title:   "fcsPurchaseDocument",
+				Version: "4.2",
+			},
+		},
+		Tst{
+			Input: `<ns2:contractProcedure xsi:type="ns3:zfcs_contractProcedureType" schemeVersion="4.5" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">`,
+			Expected: ExportInfo{
+				Title:   "contractProcedure",
+				Version: "4.5",
+			},
+		},
+		Tst{
+			Input: `<tenderPlan schemeVersion="1.0">`,
+			Expected: ExportInfo{
+				Title:   "tenderPlan",
+				Version: "1.0",
+			},
+		},
+		Tst{
+			Input: `<ns2:sketchPlan schemeVersion="1.0" xmlns="http://zakupki.gov.ru/oos/types/1" xmlns:ns2="http://zakupki.gov.ru/oos/printform/1">`,
+			Expected: ExportInfo{
+				Title:   "sketchPlan",
+				Version: "1.0",
+			},
+		},
+		Tst{
+			Input: `<contract schemeVersion="4.5">`,
+			Expected: ExportInfo{
+				Title:   "contract",
+				Version: "4.5",
+			},
+		},
 	}
 
 	for i, test := range tests {
